5ea1e78c7de53140757a158103bb718fe264c15f
Sisi Vieira <cicy886@gmail.com>
2021-08-13 20:24:39 -0700

    create CurrencyService class for separated API logic

---
+++ src/currency-service.js
@@ -0,0 +1,17 @@
+export default class CurrencyService {
+  static getCurrency(currencyName, amount){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/usd/${currencyName}/${amount}`;
+      request.onload = function(){
+        if (this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currency-service.js';
 
 $(document).ready(function() {
 });
