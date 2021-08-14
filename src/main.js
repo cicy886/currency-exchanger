99434c818f1d15c4b61b65c8c0d1befdc8f285e5
Sisi Vieira <cicy886@gmail.com>
2021-08-13 20:40:57 -0700

    add promise.then

--- src/main.js
+++ src/main.js
@@ -8,5 +8,12 @@ $(document).ready(function() {
   $('#currencyExchange').click(function(){
     let currencyName = $('#anotherCurrency').val();
     let amount = $('#amount').val();
+    let promise = CurrencyService.getCurrency(currencyName,amount);
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      $('.showCurrency').text(`You want to exchange ${amount} USD to ${currencyName}, the converstion result is ${body.conversion_result}.`);
+    }, function (error){
+      $('showErrors').text(`There was an error processing your request : ${error}`);
+    });
   });
 });
