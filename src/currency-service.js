858c38649e7a2bc5c53ab5ab53873e0aa94dbd91
Sisi Vieira <cicy886@gmail.com>
2021-08-13 22:00:57 -0700

    show error if the amount is not a number

--- src/currency-service.js
+++ src/currency-service.js
@@ -2,6 +2,7 @@ export default class CurrencyService {
   static getCurrency(currencyName, amount){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
+      console.log(request);
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/usd/${currencyName}/${amount}`;
       request.onload = function(){
         if (this.status === 200){
--- src/main.js
+++ src/main.js
@@ -7,13 +7,16 @@ import CurrencyService from './currency-service.js';
 $(document).ready(function() {
   $('#currencyExchange').click(function(){
     let currencyName = $('#anotherCurrency').val();
-    let amount = $('#amount').val();
+    let amount = parseFloat($('#amount').val());
+    if (isNaN(amount) || amount<0){
+      $('.showErrors').text('Error! This is not a valid number');
+    }
     let promise = CurrencyService.getCurrency(currencyName,amount);
     promise.then(function(response){
       const body = JSON.parse(response);
       $('.showCurrency').text(`You want to exchange ${amount} USD to ${currencyName}, the converstion result is ${body.conversion_result}.`);
     }, function (error){
-      $('showErrors').text(`There was an error processing your request : ${error}`);
+      $('.showErrors').text(`There was an error processing your request : ${error}`);
     });
   });
 });
